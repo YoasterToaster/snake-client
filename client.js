a47ae848cbb491ccbe26cdff1727fb000aeceeec
YoasterToaster <joefoschia@gmail.com>
2023-11-22 15:54:04 -0500

    Displays my name on the server now using client.js

--- client.js
+++ client.js
@@ -9,6 +9,10 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    // console.log("Successfully connected to game server");
+    conn.write("Name: SAK");
+  });
 
   conn.on("data", (data) => {
     console.log(data);
